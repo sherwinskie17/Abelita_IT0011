c156209244043c9ea55121aaa7035b7b6f84423e
sherwinskie17 <sherwinabelita1@gmail.com>
2025-02-08 23:09:51 +0800

    Technical2

--- Lab1/for3a.py
+++ Lab1/for3a.py
@@ -3,7 +3,7 @@ def pattern_a():
     rows = 5  # Number of rows in the pattern
     for i in range(1, rows + 1):  # Outer loop for rows
         # Print leading spaces
-        for j in range(rows - i):
+        for _ in range(rows - i):
             print(" ", end="")
         # Print numbers
         for k in range(1, i + 1):
